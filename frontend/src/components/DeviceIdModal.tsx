70cfb622525bd5e1e5ea0f4b1f685ebc0d15d4cf
Yassen <yassen.mladenov@gmail.com>
2026-07-09 12:23:15 +0300

    Update DeviceIdModal.tsx

--- frontend/src/components/DeviceIdModal.tsx
+++ frontend/src/components/DeviceIdModal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Modal, View, Text, Pressable, StyleSheet, Clipboard, TextInput } from "react-native";
+import { Modal, View, Text, Pressable, StyleSheet, TextInput } from "react-native";
+import * as Clipboard from "expo-clipboard";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getDeviceId, setDeviceId } from "@/src/utils/deviceId";
 import { colors, spacing, radius } from "@/src/theme/colors";
